e6bfe90470a798d41a51eeb36ef89c8da831856b
narF <narF.snw@gmail.com>
2018-03-24 19:34:11 -0400

    code formatting in !helpmore command

--- bot.js
+++ bot.js
@@ -230,15 +230,15 @@ bot.on('message', function (username, userID, channelID, message, event) {
 			case 'helpmore':
 				bot.sendMessage({
 					to: channelID,
-					message: `<@${userID}> Light bot will enlight your day by generating pretty images that are unique to you. Every time you call it with the \`!light\` command, your image will evolve. You can (and should!) call it every 5 minutes, which is how long it takes to generate your new image.
-**Commands:**
-\`!light\` Request your image. Watch it grow!
-\`!level\` Tell you your current level.
-\`!relight\` For when you've reached the end.
-\`!link\` To get the URL link to the original version of this game.
-\`!invite\` To get an URL to invite Light Bot to your own Discord server.
-\`!helpadmin\` Help about admin commands.
-You can also play the app version of this light game at https://narf.itch.io/light-game `
+					message: `<@${userID}> Light bot will enlight your day by generating pretty images that are unique to you. Every time you call it with the \`!light\` command, your image will evolve. You can (and should!) call it every 5 minutes, which is how long it takes to generate your new image.\n`+
+					"**Commands:**\n"+
+					"`!light` Request your image. Watch it grow!\n"+
+					"`!level` Tell you your current level.\n"+
+					"`!relight` For when you've reached the end.\n"+
+					"`!link` To get the URL link to the original version of this game.\n"+
+					"`!invite` To get an URL to invite Light Bot to your own Discord server.\n"+
+					"`!helpadmin` Help about admin commands.\n"+
+					"You can also play the app version of this light game at https://narf.itch.io/light-game "
 				});
 				logger.info(`More help requested by ${username}`);
 			break;
