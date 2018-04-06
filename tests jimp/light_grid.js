290925999efa3cddb16faa601e1818a46bcfb5f8
narF <narF.snw@gmail.com>
2018-04-06 00:48:32 -0400

    working on .fillGrid

--- tests jimp/light_grid.js
+++ tests jimp/light_grid.js
@@ -2,20 +2,24 @@
 
 class LightGrid {
    constructor(size) {
-      this.grid = new Array(size).fill(
-         new Array(size).fill("o")
-      );
+      this.grid = [];
+      for (var x = 0; x<size; x++) {
+         this.grid[x] = [];
+         for (var y=0; y<size; y++) {
+            this.grid[x][y] = " ";
+         }
+      }
    }
 
    indexToXY(i) {
       if (i%1 != 0) {
-         throw notAnInt;
+         throw notAnInteger;
       }
-      if (i < Math.pow(this.size,2) && i>=0) {
+      if (i < this.area && i>=0) {
          var x;
          var y;
-         y = Math.floor(i/this.size);
-         x = i % this.size;
+         y = Math.floor(i/this.length);
+         x = i % this.length;
          return [x, y];
       } else {
          throw outOfGridError
@@ -23,16 +27,56 @@ class LightGrid {
    }
 
    xyToIndex(x, y) {
-      if (x < this.size && y < this.size) {
-         return x + y*this.size;
+      if (x < this.length && y < this.length) {
+         return x + y*this.length;
       } else {
          throw outOfGridError;
       }
 
    }
 
-   get size() {
+   get length() {
       return this.grid.length;
+   }
+
+   get area() {
+      return Math.pow(this.grid.length, 2);
+   }
+
+   cellXY(x, y) {
+      return this.grid[y][x];
+   }
+
+   cellAt(i) {
+      var pos = indexToXY(i);
+      var x = pos[0];
+      var y = pos[1];
+      return this.cellXY(x, y);
+   }
+
+   setCellXY(x, y, value) {
+      this.grid[y][x] = value;
+   }
+
+   setCellAt(i, value) {
+      var pos = this.indexToXY(i);
+      var x = pos[0];
+      var y = pos[1];
+      this.setCellXY(x, y, value);
+   }
+
+   fillGrid(n, a, b) { // fills n random cells of the grid with a. The others with b
+      // TODO some verifications about size and outOfGridError
+      var randomList = [];
+
+      // Generate a list of random numbers (only once each)
+      for (var i = 0; i<this.area; i++) {
+         var randomPosition = Math.round(Math.random(randomList.length));
+         randomList.splice(randomPosition, 0, i); // insert i à un position random dans la liste
+      }
+
+
+      console.log(randomList);
    }
 }
 
@@ -41,3 +85,8 @@ class LightGrid {
 var g = new LightGrid(3);
 console.log("(1, 2) =  7 = "+g.xyToIndex(1, 2));
 console.log("7ème de 3x3 = (1,2) = "+g.indexToXY(7));
+console.log("area = 9 = "+g.area);
+console.log("length = 3 = "+g.length);
+
+console.log("g.fillGrid(5, true, false): ");
+g.fillGrid(5, true, false);
