5852c4f93052e8a94f2516812516393b05ea4304
narF <narF.snw@gmail.com>
2018-09-03 00:13:12 -0400

    test jimp outputs to a separate folder

--- tests jimp/test LightPicture.js
+++ tests jimp/test LightPicture.js
@@ -8,7 +8,7 @@ const startLevel = 1; // How many pictures to ouput
 const endLevel = 20;
 
 for (var i = startLevel; i <= endLevel; i++){
-   var lp = new LightPicture(i+1, `test${i}.png`, (err, res)=>{
+   var lp = new LightPicture(i+1, `output/test${i}.png`, (err, res)=>{
       console.log(`wrote to ${res.path}`);
    });
 }
