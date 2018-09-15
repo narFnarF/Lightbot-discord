1c08950fa861c6419ed0e82b2326214b846ea134
narF <narF.snw@gmail.com>
2018-09-15 17:13:02 -0400

    PlayerManager read DB

--- PlayerManager.js
+++ PlayerManager.js
@@ -1,15 +1,28 @@
 "use strict";
 
 // Dependencies
+const fs = require("fs");
 const Player = require('./Player.js');
 
 class PlayerManager {
    constructor(pathToDB) {
       this.pathToDB = pathToDB;
+      this.players = this.readDBFile(pathToDB);
    }
 
-   get pathToDB() {
-      return this.pathToDB;
+   // get pathToDB() {
+   //    return this.pathToDB;
+   // }
+
+   readDBFile(path) {
+      var txt = fs.readFileSync(path);
+      var content = JSON.parse(txt);
+
+      // Convert the old DB style into the new one
+      if (content.hasOwnProperty("players")) {
+         content = content.players;
+      }
+      return content;
    }
 }
 module.exports = PlayerManager;
--- playground.js
+++ playground.js
@@ -1,18 +1,18 @@
 "use strict";
 
-var logger = require('winston');
+var logger = require('./logger.js');
 var fs = require('fs');
+const PlayerManager = require("./PlayerManager.js")
 
 test();
 
 function test(){
 	logger.info("Hello! This is a test.");
 	logger.info("Hi Atest! I'm Dad!");
-	testError();
 
-}
-
-
-function testError() {
-	throw "error";
+	const pm = new PlayerManager("playersDB correct copy.json");
+	// const player = pm.fetchOrCreatePlayer("123456789asdfasdf");
+	console.log(pm);
+	// logger.info(pm);
+	// logger.info(`player: ${player}`);
 }
