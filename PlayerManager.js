525b2f5b58b074e01fdc850c4c7cec8147beaa5d
narF <narF.snw@gmail.com>
2018-09-15 23:07:27 -0400

    tabs for the win

--- PlayerManager.js
+++ PlayerManager.js
@@ -3,26 +3,53 @@
 // Dependencies
 const fs = require("fs");
 const Player = require('./Player.js');
+const logger = require("./logger.js")
 
 class PlayerManager {
-   constructor(pathToDB) {
-      this.pathToDB = pathToDB;
-      this.players = this.readDBFile(pathToDB);
-   }
+	constructor(pathToDB) {
+		this.pathToDB = pathToDB;
+		this.players = this.readDBFile(pathToDB);
+		this.currentlyWriting = false;
 
-   // get pathToDB() {
-   //    return this.pathToDB;
-   // }
+		this.writeDBFile();
+	}
 
-   readDBFile(path) {
-      var txt = fs.readFileSync(path);
-      var content = JSON.parse(txt);
+	// get pathToDB() {
+	//	 return this.pathToDB;
+	// }
 
-      // Convert the old DB style into the new one
-      if (content.hasOwnProperty("players")) {
-         content = content.players;
-      }
-      return content;
-   }
+	readDBFile(path) {
+		var txt = fs.readFileSync(path);
+		var content = JSON.parse(txt);
+
+		// Convert the old DB style into the new one
+		if (content.hasOwnProperty("players")) {
+			content = content.players;
+		}
+		return content;
+	}
+
+	writeDBFile(callback) {
+		//write the db in file
+		if (!this.currentlyWriting) { // It is unsafe to use fs.writeFile() multiple times on the same file without waiting for the callback. https://nodejs.org/api/fs.html#fs_fs_writefile_file_data_options_callback
+			this.currentlyWriting = true;
+			var beautifulPlayersDB = JSON.stringify(this.players, null, 4);
+			fs.writeFile(this.pathToDB, beautifulPlayersDB, 'utf8', (err)=>{
+				this.currentlyWriting = false;
+				if (err) {
+					logger.warn(`Could not write ${path} on disk.`);
+					logger.warn(e);
+				} else {
+					logger.debug("Saved the DB");
+				}
+				if (callback) {
+					callback(err);
+				}
+			});
+		}
+	}
+
+
+
 }
 module.exports = PlayerManager;
