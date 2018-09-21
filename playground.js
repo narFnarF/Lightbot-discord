ba4492eabc46beaad2cf94dd1613ce4c5abd5b57
narF <narF.snw@gmail.com>
2018-09-21 00:41:44 -0400

    some tests for PlayerManager and Player

--- playground.js
+++ playground.js
@@ -7,33 +7,61 @@ const PlayerManager = require("./PlayerManager.js")
 
 play();
 learning();
+runTests(true);
 
 function play(){
 	logger.info("Hello! This is a test.");
 	logger.info("Hi Atest! I'm Dad!");
 
-	const pm = new PlayerManager("playersDB correct copy.json");
-	// const player = pm.fetchOrCreatePlayer("123456789asdfasdf");
-	// console.log(pm);
-
-	logger.debug(`Does player bullshit exists? Should be false: ${pm.exists("bullshit")}`);
-	pm.createPlayer("bullshit", "bullshit_name")
-	logger.debug(`Does player bullshit exists? Should be true: ${pm.exists("bullshit")}`);
-	// console.log(pm);
-	logger.info(`name should be bullshit_name: ${pm.getPlayer("bullshit").name}`)
-	logger.info(`name should be narF: ${pm.getPlayer("214590808727355393").name}`)
-
-	// console.log (pm.getPlayer("bullshit"))
-	// console.log (pm.getPlayer("214590808727355393"))
-
-
-	// console.log(pm.getPlayer("bullshit").getName());
-	// console.log(pm.getPlayer("214590808727355393").getName());
-	// pm.writeDBFile((err)=>{
-	// 	logger.info(`done writing the DB with err:${err}`)
-	// })
 }
 
 function learning() {
 
+}
+
+
+function test(left, right, nb) {
+	var verbose = false;
+	var result = (left == right);
+	if (verbose) {
+		logger.debug(`Test #${nb} ${result}  ${left} == ${right}`);
+	}
+
+	if (!result) {
+		logger.warn(`Test #${nb} Result: ${left} != ${right}` );
+	}
+}
+
+function runTests() {
+	const pm = new PlayerManager("playersDB correct copy.json");
+
+	test(pm.exists("bullshit"), false, 1);
+	pm.createPlayer("bullshit", "bullshit_name")
+	test(pm.exists("bullshit"), true, 2);
+
+	// console.log(pm);
+	test(pm.getPlayer("bullshit").name, "bullshit_name", 3)
+	test(pm.getPlayer("214590808727355393").name, "narF", 4)
+
+	var p = pm.getOrCreatePlayer("new", "newName");
+	test(p.name, "newName", 5)
+	var p = pm.getOrCreatePlayer("bullshit");
+	test(p.name, "bullshit_name", 6)
+
+	p.increaseLevel()
+	test(p.level, 2, 7)
+
+	test(p.allowedToPlay(), true, 8)
+	p.updateLastPlayed();
+	test(p.allowedToPlay(), false, 9)
+
+	test(p.relight, undefined, 10)
+	p.increaseRelightCount()
+	test(p.relight, 1, 11)
+
+	// pm.writeDBFile((err)=>{
+	// 	logger.info(`done writing the DB with err:${err}`)
+	// })
+
+	logger.debug(`All tests completed. Is there any warnings?`)
 }
