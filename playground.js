2c90ef7094f8167a95f530aed1d5d25eb8fa5e7a
narF <narF.snw@gmail.com>
2018-09-22 19:06:27 -0400

    plus de tests et plein de ; manquants

--- playground.js
+++ playground.js
@@ -3,11 +3,12 @@
 var logger = require('./logger.js');
 var fs = require('fs');
 const PlayerManager = require("./PlayerManager.js")
+const config = require("./config.json")
 // const Player = require("./Player.js")
 
 play();
 learning();
-runTests(true);
+runTests();
 
 function play(){
 	logger.info("Hello! This is a test.");
@@ -22,46 +23,60 @@ function learning() {
 
 function test(left, right, nb) {
 	var verbose = false;
-	var result = (left == right);
+	var successful = (left === right);
 	if (verbose) {
-		logger.debug(`Test #${nb} ${result}  ${left} == ${right}`);
+		logger.debug(`Test #${nb} ${successful}  ${left} == ${right}`);
 	}
 
-	if (!result) {
+	if (!successful) {
 		logger.warn(`Test #${nb} Result: ${left} != ${right}` );
 	}
+	return successful;
 }
 
 function runTests() {
-	const pm = new PlayerManager("playersDB correct copy.json");
+	new PlayerManager("playersDB correct copy.json")
+	try {
+		var emptyPM = new PlayerManager();
+	} catch (err) {
+		// logger.info("All according to plan.")
+		// console.log(emptyPM);
+		test(emptyPM, undefined, 0.5);
+	}
+	const pm = new PlayerManager("playersDB correct copy.json", config.ownerAdmin.discordID);
+
 
 	test(pm.exists("bullshit"), false, 1);
-	pm.createPlayer("bullshit", "bullshit_name")
+	pm.createPlayer("bullshit", "bullshit_name");
 	test(pm.exists("bullshit"), true, 2);
 
 	// console.log(pm);
-	test(pm.getPlayer("bullshit").name, "bullshit_name", 3)
-	test(pm.getPlayer("214590808727355393").name, "narF", 4)
+	test(pm.getPlayer("bullshit").name, "bullshit_name", 3);
+	test(pm.getPlayer("214590808727355393").name, "narF", 4);
+	test(pm.getPlayer("existe pas"), undefined, 4.5);
 
 	var p = pm.getOrCreatePlayer("new", "newName");
-	test(p.name, "newName", 5)
+	test(p.name, "newName", 5);
 	var p = pm.getOrCreatePlayer("bullshit");
-	test(p.name, "bullshit_name", 6)
+	test(p.name, "bullshit_name", 6);
 
-	p.increaseLevel()
-	test(p.level, 2, 7)
+	p.increaseLevel();
+	test(p.level, 2, 7);
 
-	test(p.allowedToPlay(), true, 8)
+	test(p.allowedToPlay(), true, 8);
 	p.updateLastPlayed();
-	test(p.allowedToPlay(), false, 9)
+	test(p.allowedToPlay(), false, 9);
 
-	test(p.relight, undefined, 10)
-	p.increaseRelightCount()
-	test(p.relight, 1, 11)
+	test(p.relight, undefined, 10);
+	p.increaseRelightCount();
+	test(p.relight, 1, 11);
 
-	// pm.writeDBFile((err)=>{
-	// 	logger.info(`done writing the DB with err:${err}`)
-	// })
+	test( pm.isAdmin("bullshit"), false, 12);
+	test( pm.isAdmin("214590808727355393"), true, 13);
 
-	logger.debug(`All tests completed. Is there any warnings?`)
+	pm.writeDBFile((err)=>{
+		logger.info(`done writing the DB with err:${err}`)
+	})
+
+	logger.debug(`All tests completed. Est-ce que y'avait des warnings?`);
 }
