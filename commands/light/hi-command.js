b6e52d4e4726e47b1c9cbc1b506e86297166ea6b
narF <narF.snw@gmail.com>
2018-12-01 12:08:14 -0500

    [major] !hi command

---
+++ commands/light/hi-command.js
@@ -0,0 +1,22 @@
+const commando = require('discord.js-commando');
+const stripIndents = require('common-tags').stripIndents;
+// const oneLine = require('common-tags').oneLine;
+
+module.exports = class CommandHi extends commando.Command {
+	constructor(client) {
+		super(client, {
+			name: 'hi',
+			aliases: ['hello', 'salut', 'bonjour', 'test'],
+			group: 'light',
+			memberName: 'hi',
+			description: "See welcome message. Let Lightbot introduce itself.",
+			details: `Let Lightbot introduce itself.`,
+			// examples: ['join-lightbot']
+		});
+	}
+
+	async run(msg, args) {
+
+		return msg.reply(`Hello! I'm **Light Bot**. Request an image by typing \`!light\` in the chat. Like a desk plant, your image is persistant and evolves over time. You can type \`!helpmore\` for additional details. Embrace \`!light\` in your days and reach enlightenment!`);
+	}
+};
