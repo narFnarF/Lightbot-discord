3107d52c6501c6cf43c075b16391854f5c09c266
narF <narF.snw@gmail.com>
2019-05-12 18:01:16 -0400

    [minor] Adding logger to !prout

--- commands/learning/prout.js
+++ commands/learning/prout.js
@@ -1,5 +1,6 @@
 const commando = require('discord.js-commando');
 const oneLine = require('common-tags').oneLine;
+const logger = require("../../logger.js");
 
 module.exports = class ProutCommand extends commando.Command {
 	constructor(client) {
@@ -20,6 +21,7 @@ module.exports = class ProutCommand extends commando.Command {
 	}
 
 	async run(msg, args) {
+		logger.info(`${msg.author.username} a pété! 💩 (in server "${msg.guild.name}").`);
 		// const total = args.numbers.reduce((prev, arg) => prev + parseFloat(arg), 0);
 		// return msg.reply(`${args.numbers.join(' + ')} = **${total}**`);
 		// console.log(`yo ${msg.author}`);
